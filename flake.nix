a5c60f1a87841e4ae462da686f4d5e53967b6fad
hugosenari <hugosenari@gmail.com>
2022-03-12 03:14:53 -0300

    chore: re index madam

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = ''Local webserver for Design Prototyping and Front-end Development'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs."madam-main".url = "path:./main";
+  inputs."madam-main".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."madam-main".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib"];
+  in lib.mkProjectOutput {
+    inherit self nixpkgs;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+    refs = builtins.removeAttrs inputs args;
+  };
+}
